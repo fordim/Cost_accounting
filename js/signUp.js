83200ae16079d0068b5dd6019bd7e256d0a2ed8e
Fordim <dvformos@gmail.com>
2019-06-17 10:01:11 +0300

    made pages: signIn, signUp, Cabinet, main

---
+++ js/signUp.js
@@ -0,0 +1,13 @@
+
+var onPageLoaded = function() {
+
+    document.getElementById("nameInput").onkeypress = function (event) {
+        event = event || window.event;
+        if (event.charCode && (event.charCode < 65 || event.charCode > 90) && (event.charCode < 97 || event.charCode > 122)) {
+            return false;
+        }
+    };
+
+};
+
+document.addEventListener('DOMContentLoaded', onPageLoaded);
