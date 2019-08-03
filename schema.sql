ce5c27e1bb99381dfc8f7aa3ac9c6d7262261ed0
fordim <dvformos@gmail.com>
2019-08-03 20:14:27 +0300

    Made changes that was discussed on the call

--- schema.sql
+++ schema.sql
@@ -1,59 +1,35 @@
+DROP DATABASE IF EXISTS accounting;
 CREATE DATABASE accounting;
 
 use accounting;
 
 CREATE TABLE users
 (
-    id INT AUTO_INCREMENT,
-    email VARCHAR(50) UNIQUE CHECK(Email !=''),
-    name VARCHAR(30),
-    password VARCHAR(100),
-    created_at DATETIME,
-    updated_at DATETIME,
-    deleted_at DATETIME,
-    PRIMARY KEY (Id)
+    id INT PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    password_hash CHAR(60) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NULL DEFAULT NULL,
+    deleted_at DATETIME NULL DEFAULT NULL
 );
 
 CREATE TABLE categories
 (
-    id INT AUTO_INCREMENT,
-    name VARCHAR(30),
-    PRIMARY KEY (Id)
+    id INT PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL
 );
 
 CREATE TABLE history
 (
-    id INT AUTO_INCREMENT,
-    user_id INT,
-    category_id INT,
-    sum FLOAT CHECK(sum !=''),
-    comment VARCHAR(100),
-    created_at DATETIME,
-    updated_at DATETIME,
-    deleted_at DATETIME,
-    PRIMARY KEY (Id),
-    FOREIGN KEY (user_id)  REFERENCES users (id),
+    id INT PRIMARY KEY,
+    user_id INT NOT NULL,
+    category_id INT NOT NULL,
+    amount FLOAT NOT NULL,
+    comment TEXT,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NULL DEFAULT NULL,
+    deleted_at DATETIME NULL DEFAULT NULL,
+    FOREIGN KEY (user_id)  REFERENCES users (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
     FOREIGN KEY (category_id)  REFERENCES categories (id)
 );
-
-
-# Изменяем тип столбца
-ALTER TABLE history MODIFY created_at DATETIME;
-
-# Добавляем Primary Key в существующую таблицу
-ALTER TABLE categories add PRIMARY KEY (id);
-
-# Добавляем FOREIGN Key в существующую таблицу
-ALTER TABLE history add FOREIGN KEY (user_id) REFERENCES users (id);
-
-# Добавляем строки в таблицу user
-INSERT users(email, name, password, created_at, updated_at)
-VALUES ('test@mail.ru', 'Jack', '$2y$10$8cGDnK4bvKq0X65fC/m1duQtA6NuMBf5az3mvzaXARBqhNLJ21aoG', '2019-07-16 10:10:15', '2019-07-16 20:15:15');
-
-# Добавляем строки в таблицу categories
-INSERT users(name)
-VALUES ('Строительные материалы');
-
-# Добавляем строки в таблицу history
-INSERT history(user_id, category_id, sum, comment, created_at, updated_at)
-VALUES (1, 1, 500.48, 'Test comment, Test','2019-07-16 10:10:15', '2019-07-16 20:15:15');
