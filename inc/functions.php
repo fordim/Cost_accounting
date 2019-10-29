f52683baa05cddc3802e8a82b623b6a07a497d52
Fordim <dvformos@gmail.com>
2019-10-30 00:35:59 +0300

    Fix, used fputcsv instead of my big function

--- inc/functions.php
+++ inc/functions.php
@@ -158,48 +158,59 @@ function downloadAllHistory($link, int $userId){
     file_force_download('temp/test.csv');
 }
 
-function create_csv_file(array $arrayForCSV, $file = 'temp/test.csv', string $col_delimiter = ';', string $row_delimiter = "\r\n"){
-    if( ! is_array($arrayForCSV))
-        return false;
+//function create_csv_file(array $arrayForCSV, $file = 'temp/test.csv', string $col_delimiter = ';', string $row_delimiter = "\r\n"){
+//    if( ! is_array($arrayForCSV))
+//        return false;
+//
+//    if($file && ! is_dir(dirname($file)))
+//        return false;
+//
+//    $CSV_str = '';
+//
+//    foreach($arrayForCSV as $row ){
+//        $cols = array();
+//
+//        foreach($row as $col_val){
+//            // строки должны быть в кавычках ""
+//            // кавычки " внутри строк нужно предварить такой же кавычкой "
+//            if( $col_val && preg_match('/[",;\r\n]/', $col_val) ){
+//                // поправим перенос строки
+//                if( $row_delimiter === "\r\n" ){
+//                    $col_val = str_replace( "\r\n", '\n', $col_val );
+//                    $col_val = str_replace( "\r", '', $col_val );
+//                }
+//                elseif( $row_delimiter === "\n" ){
+//                    $col_val = str_replace( "\n", '\r', $col_val );
+//                    $col_val = str_replace( "\r\r", '\r', $col_val );
+//                }
+//                $col_val = str_replace( '"', '""', $col_val ); // предваряем "
+//                $col_val = '"'. $col_val .'"'; // обрамляем в "
+//            }
+//            $cols[] = $col_val; // добавляем колонку в данные
+//        }
+//        $CSV_str .= implode( $col_delimiter, $cols ) . $row_delimiter; // добавляем строку в данные
+//    }
+//    $CSV_str = rtrim( $CSV_str, $row_delimiter );
+//    // задаем кодировку windows-1251 для строки
+//    if( $file ){
+//        $CSV_str = iconv( "UTF-8", "cp1251",  $CSV_str );
+//        // создаем csv файл и записываем в него строку
+//        $done = file_put_contents( $file, $CSV_str );
+//
+//        return $done ? $CSV_str : false;
+//    }
+//    return $CSV_str;
+//}
 
-    if($file && ! is_dir(dirname($file)))
-        return false;
+function create_csv_file($arrayForCSV) {
+    $file = fopen('temp/test.csv', 'w');
 
-    $CSV_str = '';
-
-    foreach($arrayForCSV as $row ){
-        $cols = array();
-
-        foreach($row as $col_val){
-            // строки должны быть в кавычках ""
-            // кавычки " внутри строк нужно предварить такой же кавычкой "
-            if( $col_val && preg_match('/[",;\r\n]/', $col_val) ){
-                // поправим перенос строки
-                if( $row_delimiter === "\r\n" ){
-                    $col_val = str_replace( "\r\n", '\n', $col_val );
-                    $col_val = str_replace( "\r", '', $col_val );
-                }
-                elseif( $row_delimiter === "\n" ){
-                    $col_val = str_replace( "\n", '\r', $col_val );
-                    $col_val = str_replace( "\r\r", '\r', $col_val );
-                }
-                $col_val = str_replace( '"', '""', $col_val ); // предваряем "
-                $col_val = '"'. $col_val .'"'; // обрамляем в "
-            }
-            $cols[] = $col_val; // добавляем колонку в данные
-        }
-        $CSV_str .= implode( $col_delimiter, $cols ) . $row_delimiter; // добавляем строку в данные
+    foreach ($arrayForCSV as $fields){
+        fputs($file, chr(0xEF) . chr(0xBB) . chr(0xBF)); // BOM (Byte Order Mark) - кодировка, что-бы понимало русский язык.
+        fputcsv($file, array_values($fields), ';', ' ');
     }
-    $CSV_str = rtrim( $CSV_str, $row_delimiter );
-    // задаем кодировку windows-1251 для строки
-    if( $file ){
-        $CSV_str = iconv( "UTF-8", "cp1251",  $CSV_str );
-        // создаем csv файл и записываем в него строку
-        $done = file_put_contents( $file, $CSV_str );
 
-        return $done ? $CSV_str : false;
-    }
-    return $CSV_str;
+    fclose($file);
 }
 
 function file_force_download($file) {
